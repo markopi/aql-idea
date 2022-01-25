9e23a325a0a4e54677d6c27b642289e35f48c1db
Marko Pipan <marko.pipan@better.care>
2022-01-25 08:59:22 +0100

    Add aql syntax highlighter

--- src/main/kotlin/com/github/markopi/ideafirstplugin/plugin/editor/AqlTextTokenTypes.kt
+++ src/main/kotlin/com/github/markopi/ideafirstplugin/plugin/editor/AqlTextTokenTypes.kt
@@ -4,6 +4,7 @@ import com.intellij.lang.ASTFactory
 import com.intellij.lang.ASTNode
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
+import org.joni.constants.internal.TokenType
 
 object AqlTextTokenTypes {
     val AQL_FILE: IElementType = object : IFileElementType("AQL_FILE", AqlLanguage) {
@@ -11,5 +12,12 @@ object AqlTextTokenTypes {
             return ASTFactory.leaf(AQL_TEXT, chameleon.chars)
         }
     }
-    val AQL_TEXT = IElementType("AQL_TEXT", AqlLanguage)
+    val AQL_TEXT = AqlElementType("AQL_TEXT")
+
+    val AQL_KEYWORD = AqlElementType("AQL.KEYWORD")
+    val AQL_STRING = AqlElementType("AQL.STRING")
+    val AQL_SYMBOL = AqlElementType("AQL.SYMBOL")
+    val AQL_NUMBER = AqlElementType("AQL.NUMBER")
+
+    class AqlElementType(debugName: String): IElementType(debugName, AqlLanguage)
 }
--- src/main/kotlin/com/github/markopi/ideafirstplugin/plugin/editor/antlr/ErrorReportingAqlLexer.kt
+++ src/main/kotlin/com/github/markopi/ideafirstplugin/plugin/editor/antlr/ErrorReportingAqlLexer.kt
@@ -4,15 +4,33 @@ import com.marand.thinkehr.aql.antlr.AqlLexer
 import org.antlr.runtime.CharStream
 import org.antlr.runtime.RecognitionException
 import org.antlr.runtime.RecognizerSharedState
+import org.antlr.runtime.Token
 
 class ErrorReportingAqlLexer(input: CharStream, state: RecognizerSharedState = RecognizerSharedState()) :
     AqlLexer(input, state) {
 
     val errors: MutableList<AqlValidationError> = ArrayList()
+    private var peekedToken: Token? = null
 
     override fun displayRecognitionError(tokenNames: Array<String>?, e: RecognitionException) {
         val length = if (e.token != null) e.token.text.length else 1
         val token = if (e.token != null) e.token.text else null
         errors.add(AqlValidationError(e.line, e.charPositionInLine, length, getErrorMessage(e, tokenNames), token))
+    }
+
+    fun peekNextToken(): Token {
+        peekedToken?.let { return it }
+        val t = nextToken()
+        peekedToken = t
+        return t
+    }
+
+    override fun nextToken(): Token {
+        if (peekedToken!=null) {
+            val t = peekedToken
+            peekedToken=null
+            return t!!
+        }
+        return super.nextToken()
     }
 }
